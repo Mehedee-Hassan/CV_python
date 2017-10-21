16ae80949c910dd166373b4496cde20a59e2a46d
Mehedee-Hassan <mehedee.hassan@outlook.com>
2017-10-21 21:45:23 +0600

    init OpenCV with Python By Example as OCV_PyEx

--- opencv_py_example/test1.py
+++ opencv_py_example/test1.py
@@ -1,3 +1,4 @@
+
 import cv2
 import numpy as np
 img = cv2.imread('input.jpg')
@@ -8,3 +9,4 @@ rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), 30, 1)
 img_rotation = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
 cv2.imshow('Rotation', img_rotation)
 cv2.waitKey()
+
