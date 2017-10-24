9b08daedb3707bd124a29d2aac21c3d589805390
Mehedee-Hassan <mehedee.hassan@outlook.com>
2017-10-24 15:47:06 +0600

    OCV_PyEx: ch2,vignette filter

--- opencv_py_example/vignette_filter.py
+++ opencv_py_example/vignette_filter.py
@@ -1,32 +1,23 @@
-
 import cv2
 import numpy as np
-
 img = cv2.imread('input2.jpg')
-
-row ,cols = img.shape[:2]
-
+rows, cols = img.shape[:2]
+# generating vignette mask using Gaussian kernels
 kernel_x = cv2.getGaussianKernel(cols,200)
 kernel_y = cv2.getGaussianKernel(rows,200)
-
-
 kernel = kernel_y * kernel_x.T
-
 mask = 255 * kernel / np.linalg.norm(kernel)
-
-
 output = np.copy(img)
 
 
+# applying the mask to each channel in the input image
 for i in range(3):
     output[:,:,i] = output[:,:,i] * mask
 
-cv2.imshow('original' ,img)
-cv2.imshow('vignette' ,output)
+
+cv2.imshow('Original', img)
+cv2.imshow('Vignette', output)
+cv2.waitKey(0)
 
 
-cv2.waitkey(0)
-
-
-
-# commit :OCV_PyEx: ch2,vignette filter
+# commit message : OCV_PyEx: ch2,vignette filter
